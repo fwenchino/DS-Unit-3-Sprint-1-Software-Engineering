82bdfad0acf163557b60c6d5044242cc138b8b44
fwenchino <7489370+fwenchino@users.noreply.github.com>
2019-08-09 13:09:29 -0500

    1st

---
+++ acme.py
@@ -0,0 +1,47 @@
+
+import unittest
+import random
+
+class Product(object):
+    def __init__(self, name, price=10, weight=20, flammability=0.5, identifier= random.randint(1000000, 9999999)):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = identifier
+
+    def stealability(self):
+      steal_factor = self.price/self.weight
+      if steal_factor < 0.5 :
+        print('Not so stealable')
+      elif steal_factor <= 0.5 and steal_factor < 1.0  :
+        print('Kind stealable')
+      else :
+        print('Very stealable')
+    
+    def explode(self):
+      explode_f = self.weight * self.flammability
+      if explode_f < 10 :
+        print('...fizzle')
+      elif explode_f >= 10 and explode_f < 50  :
+        print('...boom')
+      else :
+        print('Very BABOOM')
+
+
+
+        class BoxingGlove(Product):
+            def __init__(self, weight = 10):
+              super().__init__(self, weight=weight)
+            
+            def explode(self):
+              print('... its a glove')
+              
+            def punch(self):
+              if self.weight <5 :
+                print('that tickels')
+              
+              elif self.weight >= 5 and self.weight < 15  :
+                print('that hurts')
+              else:
+                print('ouch mother!')
